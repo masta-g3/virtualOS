baedd0ba7187758799378b4a8b8924579fbfa162
Manuel Rueda <manolorueda@gmail.com>
2025-12-23 15:44:04 -0800

    Update serve_web.py to use port 8889 for deployment

--- serve_web.py
+++ serve_web.py
@@ -4,5 +4,5 @@ from textual_serve.server import Server
 
 if __name__ == "__main__":
     tui_path = Path(__file__).parent / "tui.py"
-    server = Server(f"{sys.executable} {tui_path}", host="0.0.0.0", port=8888)
+    server = Server(f"{sys.executable} {tui_path}", host="0.0.0.0", port=8889)
     server.serve()
